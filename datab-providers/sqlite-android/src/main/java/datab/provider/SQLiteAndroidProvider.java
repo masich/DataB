8d7a3ba08393b5f37cd14c090ccd979836c14853
Max Omelchenko <masich.dev@gmail.com>
2018-06-03 00:44:35 +0300

    Added sqlite (android) database provider

---
+++ datab-providers/sqlite-android/src/main/java/datab/provider/SQLiteAndroidProvider.java
@@ -0,0 +1,21 @@
+package datab.provider;
+
+import org.sqldroid.SQLDroidDriver;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class SQLiteAndroidProvider extends SQLiteProvider {
+    static {
+        try {
+            DriverManager.registerDriver(new SQLDroidDriver());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public String getDBStringPrefix() {
+        return SQLDroidDriver.sqldroidPrefix;
+    }
+}
